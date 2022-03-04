efb1951a607551049eabf34c3e6d203e1c1d591d
Sergey Petrov <mavericusdev@gmail.com>
2022-03-04 20:55:51 +0300

    puts the leagues-page logic in the withData HOC

--- src/pages/leagues-page/leagues-page.jsx
+++ src/pages/leagues-page/leagues-page.jsx
@@ -1,65 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { Pagination, Spin } from 'antd';
-import Search from 'antd/lib/input/Search';
+import React from 'react';
+import Search from 'antd/es/input/Search';
+import { Pagination } from 'antd';
 import withSoccerService from '../../hoc/with-soccer-service';
-import Config from '../../utils/const';
 import LeaguesList from '../../componets/leagues-list/leagues-list';
+import Config from '../../utils/const';
+import withData from '../../hoc/withData';
+import compose from '../../hoc/compose';
 
-const LeaguesPage = ({ getData }) => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [currentPage, setCurrentPage] = useState(searchParams.get('page') || 1);
-  const [leagues, setLeagues] = useState([]);
-  const [searchPhrase, setSearchPhrase] = useState(searchParams.get('q') || '');
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
-  const setCurrentSearchParams = () => {
-    setSearchParams({
-      page: currentPage,
-      q: searchPhrase,
-    });
-  };
-
-  const paginationChangeHandler = (page) => {
-    setCurrentPage(page);
-    setCurrentSearchParams();
-  };
-
-  const searchHandler = (value) => {
-    setSearchPhrase(value);
-    setCurrentSearchParams();
-  };
-
-  useEffect(() => {
-    getData()
-      .then((data) => setLeagues(data))
-      .catch(() => setIsError(true))
-      .finally(() => setIsLoading(false));
-  }, []);
-
-  const filteredLeagues = leagues
-    .filter(({ name }) => name.toLowerCase().includes(searchPhrase.toLowerCase()));
-  const firstElementToCurrentPage = (currentPage - 1) * Config.COUNT_ITEMS_PER_PAGE;
-  const lastElementToCurrentPage = firstElementToCurrentPage + Config.COUNT_ITEMS_PER_PAGE;
-  const leaguesToShow = filteredLeagues
-    .slice(firstElementToCurrentPage, lastElementToCurrentPage);
-
-  if (isLoading) {
-    return <Spin />;
-  }
-
-  if (isError) {
-    return <div>errror</div>;
-  }
+const LeaguesPage = (props) => {
+  const {
+    data, paginationChangeHandler, searchChangeHandler, currentPage, searchPhrase, count,
+  } = props;
 
   return (
     <div className="container">
-      <Search placeholder="input search text" onSearch={searchHandler} style={{ width: 200 }} defaultValue={searchPhrase} />
-      <LeaguesList leagues={leaguesToShow} />
+      <Search
+        placeholder="input search text"
+        onSearch={searchChangeHandler}
+        style={{ width: 200 }}
+        defaultValue={searchPhrase}
+      />
+      <LeaguesList leagues={data} />
       <Pagination
         defaultCurrent={currentPage}
-        total={filteredLeagues.length}
+        total={count}
         pageSize={Config.COUNT_ITEMS_PER_PAGE}
         onChange={paginationChangeHandler}
       />
@@ -71,4 +35,7 @@ const mapMethodsToProps = (soccerService) => ({
   getData: soccerService.getLeagues,
 });
 
-export default withSoccerService(mapMethodsToProps)(LeaguesPage);
+export default compose(
+  withSoccerService(mapMethodsToProps),
+  withData,
+)(LeaguesPage);
