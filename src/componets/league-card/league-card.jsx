a95d46fa416f469e62a15609c00aadc5ed13b328
Sergey Petrov <mavericusdev@gmail.com>
2022-03-04 22:14:01 +0300

    refactor leagues-list

---
+++ src/componets/league-card/league-card.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card } from 'antd';
+import './league-card.css';
+
+const LeagueCard = ({
+  id, name, emblemUrl, country,
+}) => {
+  return (
+    <Link to={`/league/${id}`}>
+      <Card className="league-card">
+        <div className="league-card__image">
+          <img src={emblemUrl} alt="" />
+        </div>
+        <h3>{name}</h3>
+        <p>{country}</p>
+      </Card>
+    </Link>
+  );
+};
+
+export default LeagueCard;
